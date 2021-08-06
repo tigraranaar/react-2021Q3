157385784849895f6a22d8b7ccca64904a11fc0c
tigraranaar <Tigran1402_3991>
2021-08-06 16:20:27 +0400

    clean master

--- index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDom from "react-dom";
-import App from "./src/components/App/App";
-import "./src/components/App/App.scss";
-
-ReactDom.render(<App />, document.getElementById("app"));
